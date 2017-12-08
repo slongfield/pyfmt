9e3afe2039789c117607b10ba88297c51df91475
Stephen Longfield <stephen.longfield@gmail.com>
2017-12-07 22:13:18 -0800

    Updating documentation

--- doc.go
+++ doc.go
@@ -3,28 +3,33 @@
 /*
 pyfmt implements Python-style advanced string formatting.
 
-This is an alternative to the `fmt` package's Sprintf-style string formatting, and duplicates the
+This is an alternative to the `fmt` package's Sprintf-style string formatting, and mimics the
 .format() style formatting available in Python >2.6.
 
-Braces {} are used to indicate 'fields', anything outside of braces will be emitted directly into
-the output string, and anything inside will be used to get values from the other function arguments,
-and format them. The one exception is double braces, '{{' and '}}', which will cause literal '{' and
-'}' runes to be emitted in the output.
+Braces {} are used to indicate 'format items', anything outside of braces will be emitted directly
+into the output string, and anything inside will be used to get values from the other function
+arguments, and format them. The one exception is double braces, '{{' and '}}', which will cause
+literal '{' and '}' runes to be emitted in the output.
 
-Each field consists of a 'field name', which indicates which value from the argument list to use,
+Each format item consists of a 'field name', which indicates which value from the argument list to
+use, and a 'format specifier', which indicates how to format that item.
 
 Getting Values from Field Names
+
+Values can be fetched from field names in two forms: simple names, or compound names. All compound
+names build off of simple names, and all simple names are dependent on the type of format function
+you call.
+
+Simple field names:
 
 TODO(slongfield): Make notes about how to get values out of lists, maps, and structs, as well as
 notes about nested lists, maps, and structs.
 
-  Simple field names
-
-  Compound field names
+Compound field names:
 
 Formatting
 
-
+Note note note
 
 Examples
 
