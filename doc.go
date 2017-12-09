a0e10c806c7d9b32cf4c259c3ddc1e4bab11985f
Stephen Longfield <stephen.longfield@gmail.com>
2017-12-09 01:16:19 -0800

    Finishing up first-pass documentation.

--- doc.go
+++ doc.go
@@ -22,17 +22,125 @@ you call.
 
 Simple field names:
 
-TODO(slongfield): Make notes about how to get values out of lists, maps, and structs, as well as
-notes about nested lists, maps, and structs.
+Simple field name lookup depends on which of the Format functions you're calling.
+
+Using the base Format function, you can lookup fields in two ways from lists, first, by {}, which
+gets the 'next' item, and second, by {n}, which gets the nth item. Accessing these two ways is
+independent, so while
+
+  Format("{} {} {}", ...)
+
+Is equivalent to
+
+  Format("{0} {1} {2}", ...)
+
+so is
+
+  Format("{} {1} {2}", ...)
+
+but
+
+  Format("{} {1} {}", ...)
+
+Is equivalent to:
+
+  Format("{0} {1} {1}". ...)
+
+Accessing an element that's outside the list range, will return an error, and MustFormat will panic.
+
+Using FormatMap, pass a map[string]interface{} to lookup names from the map using the string keys
+from the map. For instance:
+
+  FormatMap("{test}", map[string]int{"test": 5})
+
+returns
+
+  "5".
+
+Attempting to read from an undefined key will return an error.
+
+Using FormatStruct, you can reference arguments from a struct:
+
+  FormatStruct("{test}": myStruct{test: 5})
+
+returns
+
+ "5".
 
 Compound field names:
 
+If the value referenced by the field is itself a List, map[string]interface{}, or struct, it can be further accessed in the format string.
+
+Lists are accessed with square brackets:
+
+  Format("{0[0]}", []string{"test"}) -> "test"
+
+Similarly, maps are accessed with square brackets:
+
+  Format("{0[test]}", map[string]interface{}{"test": "42"}) -> "42"
+
+And struct fields are accessed with period, '.'
+
+  FormatStruct("{foo.bar.baz}", MyStruct{foo: Foo{bar: Bar{baz: "test"}}}) -> "test"
+
 Formatting
 
-Note note note
+If after a field name, there's a ':', what follows is considered to be the format specifier. If a
+type satisfies the Format interface (discussed below), the format specifier will be passed to that,
+but otherwise, it will fall back to the default formatter, which expects the standard format
+specifier:
+
+  [[fill]align][sign][#][0][minimumwidth][.precision][type]
+
+The optional align feature can be one of the following:
+
+  '<': left-aligned
+	'>': right-aligned
+	'=': padding after the sign, but before the digits (e.g., for +000042)
+	'^': centerec
+
+If an align flag is defined, a 'fill' character can also be defined. If undefined, space (' ') will
+be used.
+
+The optional 'sign' is only valid for numeric types and can be:
+
+  '+': Show sign for both positive and negative numbers
+	'-': Show sign only for negative numbers (default)
+	' ': use a leading space for positive numbers
+
+If # is present, when using the binary, octal, or hex types, a '0b', '0o', or '0x' will be
+prepended, respectively.
+
+The minimumwidth field specifies a minimum width, which is helpful when used with alignment.
+
+The precision field specifies a maximum width for non-floating point, non-integer types, and the
+number of points to show after the decimal point for floating types.
+
+The 'type' format determines what type the value will be formatted as.
+
+For integers:
+
+  'b' - Binary, base 2
+	'd' - Decimal, base 10 (default)
+	'o' - Octal, base 8
+	'x' - Hexadecimal, base 16
+	'X' - Hexadecimal, base 16, using upper-case letters
+
+For floats:
+
+  'e' - Scientific notation
+	'E' - Similar to e, but uppercase
+	'f' - Fixed point, displays number as a fixed-point number.
+	'F' - Same, but uppercase.
+	'g' - General format, prints as a fixed point unless its too large, than switches to scientific
+	      notation. (default)
+	'G' - Similar to g, but uses capital letters
+	'%' - Percentage, multiplies the number by 100 and displays it with a '%' sign.
 
 Examples
 
 TODO(slongfield): Add examples.
+
+TODO(slongfield): Details on custom formatters.
 */
 package pyfmt
