4639d99ce115e6644ded8cffc4367233b8ae4f58
Stephen Longfield <stephen.longfield@gmail.com>
2017-12-21 23:11:32 -0800

    Adding get_element tests.

---
+++ get_element_test.go
@@ -0,0 +1,31 @@
+package pyfmt
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestGetElement(t *testing.T) {
+	tests := []struct {
+		elems        []interface{}
+		lookupStr    string
+		lookupOffset int
+		want         interface{}
+	}{
+		{[]interface{}{3}, "", 0, 3},
+		{[]interface{}{3, "asdf"}, "", 1, "asdf"},
+		{[]interface{}{3}, "0", 0, 3},
+		{[]interface{}{3, "asdf"}, "1", 0, "asdf"},
+		{[]interface{}{struct{ test string }{test: "adsf"}}, "test", 0, "asdf"},
+	}
+
+	for _, test := range tests {
+		got, err := getElement(test.lookupStr, test.lookupOffset, test.elems...)
+		if err != nil {
+			t.Errorf("getElement(%v, %v, %v) Errored: %v", test.lookupStr, test.lookupOffset, test.elems, err)
+		}
+		if !reflect.DeepEqual(test.want, got) {
+			t.Errorf("getElement(%v, %v, %v) = %v Want: %v", test.lookupStr, test.lookupOffset, test.elems, got, test.want)
+		}
+	}
+}
