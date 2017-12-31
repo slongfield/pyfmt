ee0c6ba33feae7ddd8528159deaf2518cfae1aed
Stephen Longfield <stephen.longfield@gmail.com>
2017-12-30 21:12:08 -0800

    Always using fmt.Sprintf.

--- render.go
+++ render.go
@@ -150,51 +150,9 @@ func (r *render) parseFlags(flags string) error {
 }
 
 func (r *render) render() error {
-	// TODO(slongfield) Dispatch to different functions if the type is set.
-	switch t := r.val.(type) {
-	case string:
-		r.buf.WriteString(r.val.(string))
-		return nil
-	case int:
-		r.buf.WriteString(strconv.FormatInt(int64(t), 10))
-		return nil
-	case int8:
-		r.buf.WriteString(strconv.FormatInt(int64(t), 10))
-		return nil
-	case int16:
-		r.buf.WriteString(strconv.FormatInt(int64(t), 10))
-		return nil
-	case int32:
-		r.buf.WriteString(strconv.FormatInt(int64(t), 10))
-		return nil
-	case int64:
-		r.buf.WriteString(strconv.FormatInt(t, 10))
-		return nil
-	case uint:
-		r.buf.WriteString(strconv.FormatUint(uint64(t), 10))
-		return nil
-	case uint8:
-		r.buf.WriteString(strconv.FormatUint(uint64(t), 10))
-		return nil
-	case uint16:
-		r.buf.WriteString(strconv.FormatUint(uint64(t), 10))
-		return nil
-	case uint32:
-		r.buf.WriteString(strconv.FormatUint(uint64(t), 10))
-		return nil
-	case uint64:
-		r.buf.WriteString(strconv.FormatUint(t, 10))
-		return nil
-	case reflect.Value:
-		if t.IsValid() && t.CanInterface() {
-			r.val = t.Interface()
-			return r.render()
-		}
-		return r.renderValue(t)
-	default:
-		r.buf.WriteString(fmt.Sprintf("%v", r.val))
-		return nil
-	}
+	//TODO(slongfield): Create the format string.
+	r.buf.WriteString(fmt.Sprintf("%v", r.val))
+	return nil
 }
 
 func (r *render) renderValue(v reflect.Value) error {
