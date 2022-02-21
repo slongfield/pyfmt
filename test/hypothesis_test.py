2bf2b916a755592d851ba6ffcf089c4d2daf491c
Stephen Longfield <stephen.longfield@gmail.com>
2022-02-21 16:47:27 -0500

    Adding venv to Hypothesis testing environment

--- test/hypothesis_test.py
+++ test/hypothesis_test.py
@@ -6,13 +6,13 @@ pyfmt.
 """
 
 import string
-from hypothesis import assume, given, settings
+from hypothesis import assume, given, settings, HealthCheck
 from hypothesis.strategies import floats, from_regex, integers, text
 
 import build
 
 _NUM_TEST = 1000
-_DEBUG = False
+_DEBUG = True
 
 
 @given(text(alphabet=string.printable))
@@ -35,19 +35,28 @@ def test_no_format_arguments(fmt_str):
 @settings(max_examples=_NUM_TEST)
 def test_no_format_arguments_errors(fmt_str):
     """Test that without format arguments, match error conditions."""
+    if _DEBUG:
+        print("{}.format()".format(fmt_str))
+
+    # If python errors, golang should error.
     try:
         fmt_str.format().encode("ascii")
     except (ValueError, IndexError, KeyError):
         assert build.FormatNothingError(fmt_str.encode("ascii"))
         return
-    assume(False)
+
+    # If python did not error, Golang shouldn't error.
+    try:
+        build.FormatNothing(fmt_str.encode("ascii"))
+    except e:
+        assert False
 
 
 @given(text(alphabet=string.printable, max_size=10),
        from_regex(r"\A(([:print:][<>=^])|([<>=^]?))[\+\- ]?#?[0-9]{0,4}[bdoxX]\Z"),
        text(alphabet=string.printable, max_size=10),
        integers(min_value=-2**31, max_value=2**31 - 1))
-@settings(max_examples=_NUM_TEST)
+@settings(max_examples=_NUM_TEST,suppress_health_check=(HealthCheck.filter_too_much,))
 def test_format_one_int(pre_str, fmt, post_str, val):
     """Test that a single integer is formatted correctly."""
     fmt_str = pre_str + "{:" + fmt + "}" + post_str
@@ -75,7 +84,7 @@ def test_format_one_int(pre_str, fmt, post_str, val):
        from_regex(r"\A(([:print:][<>=^])|([<>=^]?))[\+\- ]?#?[0-9]{0,4}[bdoxX]\Z"),
        text(alphabet=string.printable, max_size=10),
        integers(min_value=-2**31, max_value=2**31 - 1))
-@settings(max_examples=_NUM_TEST)
+@settings(max_examples=_NUM_TEST,suppress_health_check=(HealthCheck.filter_too_much,))
 def test_format_one_int_erros(pre_str, fmt, post_str, val):
     """Test that a single integer is formatted correctly."""
     fmt_str = pre_str + "{:" + fmt + "}" + post_str
